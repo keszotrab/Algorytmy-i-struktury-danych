621fd2718e21f3b86c687a87d30af9f468072c59
keszotrab <39887624+keszotrab@users.noreply.github.com>
2022-04-12 12:23:33 +0200

    Add files via upload
    Tragedia

---
+++ Algorytmy-i-struktury-danych/jest problem/ConsoleApp1/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {/*Console.WriteLine("Hello World!");
+
+
+            List<int> banan = new List<int>();
+
+            banan.Add(1);
+            banan.Add(14);
+            banan.Add(1123);
+            banan.Add(14);
+            banan.Add(2137);
+
+            foreach (var item in banan)
+            {
+                Console.WriteLine("To : " + item);
+            }
+
+
+            //If(head.next = this.next)
+            //      return true ?
+            */
+            }
+
+
+
+            Node<string> node = new Node<string> { Value = " 12 41 24 1290 4912" };
+            node.Next = new Node<string> { Value = "     214  - 1243          912312" };
+            node.Next.Next = new Node<string> { Value = "179 53 1  124  09-121 18 " };
+            node.Next.Next.Next = node;
+            Node<string> head = node;
+            while (node != null)
+            {
+                Console.WriteLine(node.Value);
+                node = node.Next;
+            }
+
+
+
+
+
+
+        }
+        class Node<T>
+        {
+            public T Value { get; set; }
+            public Node<T> Next { get; set; }
+
+            public T Add()
+            {
+                   // ma zwracac zmienna utworzona w  klasie np[. T cokolwiek;
+                //return T Value; 
+            }
+        
+        
+        
+        }
+
+
+        
+        class bambam
+        {
+
+        }
+
+
+    }
+
+
+}
+
+
+    
+
---
+++ Algorytmy-i-struktury-danych/jest problem/ConsoleApp2/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //FIFO
+            Node<string> node = new Node<string> { Value = "adam" };
+            node.Next = new Node<string> { Value = "ewa" };
+            node.Next.Next = new Node<string> { Value = "Karol" };
+            Node<string> head = node;
+            while (node != null)
+            {
+                Console.WriteLine(node.Value);
+                node = node.Next;
+            
+            }
+            
+            
+            //LIFO
+            Stack<int> stack = new Stack<int>();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            while (!stack.IsEmpty())
+            {
+                Console.WriteLine(stack.Pop());
+            }
+
+
+            /* 
+              
+
+            z7
+
+                
+              
+              
+              
+              
+             
+             
+             
+             
+             
+             
+             
+             
+             
+             
+             */ 
+
+
+
+        }
+
+
+        class Node<T>
+        { 
+            public T Value { get; set; }
+            public Node<T> Next { get; set; }
+        }
+
+        class Stack<T>
+        {
+            public T Value { get; set; }
+            private Node<T> _head { get; set; }
+
+            public void Push(T value)
+            {
+                Node<T> node = new Node<T> { Value = value };
+                node.Next = _head;
+                _head = node;
+            }
+
+            public bool IsEmpty()
+            {
+                return _head == null;
+                
+            }
+
+            public T Pop()
+            {
+                if (IsEmpty())
+                {
+                    throw new Exception("Stack is empty!");
+                }
+
+                T result = _head.Value;
+                _head = _head.Next;
+                return result;
+
+            }
+            
+            
+        }
+
+
+
+
+
+
+    }
+}
